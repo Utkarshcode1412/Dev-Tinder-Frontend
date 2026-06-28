55fa9dbbd31f656637c5ed342ce9c6919ee9d799
Utkarshcode1412 <pawarutkarsh579@gmail.com>
2026-06-29 00:07:38 +0530

    added NavBar component and updated App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import NavBar from "./NavBar.jsx"
 
 function App() {
-  
-
   return (
     <>
-      
+       <NavBar/>      
+
+       <div className="p-10 flex flex-col items-center justify-center gap-4 min-h-screen bg-base-200">
+          <h1 className="text-3xl font-bold text-center">
+            Hi Utkarsh!!!!!
+          </h1>
+          <button className="btn btn-primary">fuck Me</button>
+        </div>
     </>
   )
 }
